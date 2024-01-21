6e83b158b0b808527f59ab56939adb044e2063b1
Marc <sjetqd@gmail.com>
2024-01-21 16:26:44 -0700

    index.vue created

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,22 @@
+<!-- pages/index.vue -->
+
 <template>
-  <main>
-    <h1>Home Page</h1>
-  </main>
+  <div>
+    <h1>Data from API:</h1>
+    <ul>
+      <li v-for="item in apiData" :key="item.id">{{ item.name }}</li>
+    </ul>
+  </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useFetch } from '@nuxt/http';
+
+const apiData = ref([]);
+
+onMounted(async () => {
+  const { data } = await useFetch('/api/disney');
+  apiData.value = data;
+});
+</script>
